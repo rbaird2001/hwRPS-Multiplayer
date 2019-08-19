4bd15716214a03d3b10a374717662949debde606
RBaird2001 <rbaird2001@yahoo.com>
2019-08-18 23:08:18 -0600

    Near full rewrite of play logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,69 +13,119 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
 let db = firebase.firestore();
 let selectionOutcomes = db.collection("selectionOutcomes");
 let rpslp = {} //short for Rock, Paper, Scissors, Lizard, sPock
 let playID = "";
-let roundID = "";
+//let roundID = "";
 let p1Played = null;
 let p2Played = null;
+let winner = ""
 
 $("#newPlay").click(function () {
-    let p1 = $("#p1Name").val()
-    let p2 = $("#p1Name").val()
+    let p1 = "Smash BuffBody" //$("#p1Name").val()
+    let p2 = "Stump BoneMeal" //$("#p2Name").val()
+    console.log(p1 + " vs " + p2);
     if (p1 && p2) {
         selectionOutcomes.get().then(function (snap) {
             snap.forEach(function (doc) {
                 rpslp[doc.id] = doc.data();
+            });
+        })
+        playID = db.collection("plays").doc("game")
+            console.log(playID);
+            playID.set({
+                player1: p1,
+                player2: p2,
+                p1Choice: "",
+                p2Choice: "",
+                pWinner: "",
+                p1Wins: 0,
+                p2Wins: 0,
             })
-        })
-        playID = db.collection("plays").doc();
-        playID.set({
-            player1: p1,
-            player2: p2,
-            p1Choice: "",
-            p2Choice: "",
-            winner: ""
-
-        })
-        newRound();
-    }
-})
-
-function newRound (){$("#newRound").click(function () {
-    roundID = playID.collection("rounds").doc();
-    roundID.set({
-        p1: "",
-        p2: "",
-        p1Choice: "",
-        p2Choice: "",
-        winner: ""
-    });
-})
-}
-roundID.onSnapshot(function (doc) {
-    if (roundID.p1Choice && roundID.p2Choice) {
-        outcome = rpslp[p1Played][p2Played]
-        if (outcome === 0) {
-            winner = p1;
-        }
-        else if (outcome === 1) {
-            winner = p2;
-        }
-        else {
-            winner = "draw"
-        }
     }
 });
 
-$("#p1Choice").click(function () {
+$("input[name='p1']").click(function () {
     p1Played = $(this).val();
-    roundID.update({ p1Choice: p1Played });
+    playID.update({ p1Choice: p1Played }).then(function(){
+        playID.onSnapshot(function(snap){
+            getWinner = snap.data().pWinner;
+            if(getWinner){
+                $("#winner").html(getWinner);
+            };
+        });
+        playID.get().then(function(doc){
+            let curPlay = doc.data();
+            let p2c = curPlay.p2Choice;
+            if(curPlay.p2Choice){
+                outcome = rpslp[p1Played][p2c]
+                if(outcome === 1){
+                    winner = curPlay.player1;
+                }
+                else if(outcome === 0){
+                    winner = curPlay.player2;
+                }
+                else {winner = "Draw";}
+                playID.onSnapshot(function(snap){
+                    $("winner").html(snap.data().winner)
+                });
+                playID.update({pWinner: winner}).then(function(){
+                    playID.onSnapshot(function(){})
+                });
+            }
+        })    
+    });
+
 });
 
-$("#p2Choice").click(function () {
+$("input[name='p2']").click(function () {
     p2Played = $(this).val();
-    roundID.update({ p2Choice: p2Played });
+    playID.update({ p2Choice: p2Played }).then(function(){
+        playID.onSnapshot(function(snap){
+            getWinner = snap.data().pWinner;
+            if(getWinner){
+                $("#winner").html(getWinner)
+            };
+        });
+        playID.get().then(function(doc){
+            let curPlay = doc.data();
+            let p1c = curPlay.p1Choice
+            console.log(p2Played)
+            if(curPlay.p1Choice){
+                let outcome = rpslp[p2Played][p1c]
+                if(outcome === 1){
+                    winner = curPlay.player2;
+                }
+                else if(outcome === 0){
+                    winner = curPlay.player1;
+                }
+                else {winner = "Draw";}  
+            }
+            playID.update({pWinner: winner})
+    
+        })    
+    });
+
 });
+
+
+// function analyzePlay(pid) {
+//     pid.onSnapshot(function (doc) {
+//         console.log("snapshot triggered")
+        // outcome = rpslp[p1Played][p2Played];
+        // console.log(outcome);
+        // if (pid.p1Choice && pid.p2Choice) {
+        //     if (outcome === 0) {
+        //         pid.update({ winner: p1 });
+        //     }
+        //     else if (outcome === 1) {
+        //         pid.update({ winner: p2 });
+        //     }
+        //     else {
+        //         pid.update({ winner: "Draw" })
+        //     }
+        // }
+        //pid.onSnapshot(function(){});
+//     });
+// }
